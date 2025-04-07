5227ded38b96bf552a1e57ace352a70c1821a390
Rohan Gautam <rohan.gautam@mca.christuniversity.in>
2025-04-08 00:00:37 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -7,13 +7,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_iRbWfZltl26EGPtA6vOt_BdurUE5b_M",
+  apiKey: "//own key",
   authDomain: "queue-d0180.firebaseapp.com",
   projectId: "queue-d0180",
   storageBucket: "queue-d0180.firebasestorage.app",
-  messagingSenderId: "58702351087",
-  appId: "1:58702351087:web:27d35f0af2b47b0698a91a",
-  measurementId: "G-70HZWEXT73"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
